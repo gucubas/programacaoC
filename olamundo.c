66051f8a60f768a315ed22b9dcf61eb0b637cdb4
gucubas <gcgusmao@gmail.com>
2026-02-28 20:01:37 -0300

    Atualizar o olamundo.c .
    teste de código printf e scanf.

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,16 @@
+#include <stfio.h> 
 
+int main(){
+
+   int numero; 
+   char nome[30];
+
+   printf("Escreva um número: \n); 
+   scanf("%d"\, &numero);
+
+   printf("Escreva seu nome: \n); 
+   scanf("%s", nome);
+
+   retorno 0;
+
+}
